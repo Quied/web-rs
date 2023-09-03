36590b692a020318a8edbb9df1aa167114a6a3a2
svyatoslav <quiedcpp@gmail.com>
2023-09-03 08:26:22 +0300

    + diesel, migrations

---
+++ src/models.rs
@@ -0,0 +1,15 @@
+use diesel::prelude::*;
+// use crate::schema::posts;
+
+#[derive(Queryable, Selectable)]
+#[diesel(table_name = crate::schema::posts)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct Post {
+    pub id: i32,
+    pub title: String,
+    pub body: String,
+    pub published: bool,
+}
+
+
+
